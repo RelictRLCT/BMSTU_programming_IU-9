afec2634bb9b3b6d52f9176ea5a3aabf91baef8a
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2022-12-29 15:09:19 +0300

    Create 9_dsort.c

---
+++ algorithms/2.Алгоритмы сортировки и поиска/9_dsort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <iso646.h>
+#include <string.h>
+#include <stdlib.h>
+
+void Dissort(char* str, long int n)
+{
+    long int chars[26]={0};
+    int j=0;
+    while(j<n)
+    {
+        chars[(int)(str[j]-'a')]+=1;
+        j+=1;
+    }
+    j=0;
+    while(j<26)
+    {
+        while(chars[j]!=0)
+        {
+            printf("%c", (char)(j+(int)'a'));
+            chars[j]-=1;
+        }
+        j+=1;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    char str[1000001];
+    gets(str);
+    Dissort(str, strlen(str));
+    return 0;
+}
