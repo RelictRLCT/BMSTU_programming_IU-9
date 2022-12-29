c3aab25307c378c97149909a34298f574d190c67
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2022-12-29 15:23:38 +0300

    Create 8_listbsort.c

---
+++ algorithms/3.Применение динамических множеств/8_listbsort.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <iso646.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Elem {
+    struct Elem *next;
+    char *word;
+};
+
+bool ListEmpty(struct Elem* l)
+{
+    return (l==NULL) ? true : false;
+}
+
+int ListLength(struct Elem* l)
+{
+    int len=0;
+    struct Elem* x=l;
+    while(x!=NULL)
+    {
+        len+=1;
+        x=x->next;
+    }
+    return len;
+}
+
+struct Elem* ListSearch(struct Elem* l, char* wor)
+{
+    struct Elem* x=l;
+    while(x!=NULL and x->word!=wor)
+        {printf("d"); x=x->next;}
+    return x;
+}
+
+struct Elem *bsort(struct Elem *list)
+{
+    int t=ListLength(list)-1;
+    while(t>0)
+    {
+        struct Elem* tmp=list;
+        int bound = t;
+        t=0;
+        int i=0;
+        while(i<bound)
+            {
+                if(strlen(tmp->next->word)<strlen(tmp->word))
+                    {
+                        char* tmpword=tmp->word;
+                        tmp->word=tmp->next->word;
+                        tmp->next->word=tmpword;
+                        t=i;
+                    }
+                i+=1;
+                tmp=tmp->next;
+            }
+    }
+    return list;
+}
+
+int main(int argc, char* argv[])
+{
+    struct Elem* l=(struct Elem*)malloc(sizeof(struct Elem)), *list;
+    l->next=NULL;
+    list=l;
+    char c=' ';
+
+        scanf("%c", &c);
+        char* wordd=(char*)malloc(1);
+        int lenn=1;
+        while(c!=' ' and c!='\n')
+        {
+            wordd[lenn-1]=c;
+            lenn+=1;
+            wordd=(char*)realloc(wordd, lenn);
+            wordd[lenn-1]='\0';
+            scanf("%c", &c);
+        }
+        list->word=wordd;
+        int guard_malloc=1;
+    char word[1001];
+    while(c!='\n')
+    {
+        struct Elem* element=(struct Elem*)malloc(sizeof(struct Elem));
+        scanf("%c", &c);
+        int len=1;
+        while(c!=' ' and c!='\n')
+        {
+            word[len-1]=c;
+            len+=1;
+            word[len-1]='\0';
+            scanf("%c", &c);
+        }
+        if(len!=1)
+        {
+            element->word=(char*)malloc(strlen(word)+1);
+            strcpy(element->word, word);
+            list->next=element;
+            list=list->next;
+        }
+        else free(element);
+    }
+    list->next=NULL;
+    bsort(l);
+    struct Elem* freee=l;
+    struct Elem* freee2=l;
+    int ccc=0;
+    while(l!=NULL)
+    {
+        freee=l;
+        printf("%s ", l->word);
+        l=l->next;
+            free(freee->word);
+        free(freee);
+        ccc+=1;
+    }
+    free(l);
+    return 0;
+}
