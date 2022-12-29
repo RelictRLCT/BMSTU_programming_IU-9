1eed770fcc9665bf71aae260a43b8c17b6007886
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2022-12-29 14:59:00 +0300

    Create 10_maxarray.c

---
+++ algorithms/1.Изучение языка C/10_maxarray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iso646.h>
+#include <string.h>
+
+int maxarray(void *base, size_t nel, size_t width,
+            int (*compare)(void *a, void *b))
+{
+    char* max=(char*)base;
+    int maxind=0;
+    for(size_t i=0; i<nel; ++i)
+    {
+       char* tmp1=(char*)base+(i*width);
+        if ((compare((void*)tmp1, (void*)max))>0)
+            {max=(char*)tmp1;
+            maxind=i;}
+    }
+    return maxind;
+}
