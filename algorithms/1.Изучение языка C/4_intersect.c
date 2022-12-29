8d20834e9a41ad2ebd2d9377ca39a0c83d84ffde
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2022-12-29 14:52:06 +0300

    Create 4_intersect.c

---
+++ algorithms/1.Изучение языка C/4_intersect.c
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <stdio.h>
+
+
+int checkbit(int value, int position) {
+    int result;
+    if ((value & (1 << position)) == 0)
+    {
+        result = 0;
+    }
+    else
+    {
+        result = 1;
+    }
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+    int32_t a, b, res;
+    int n1, n2;
+    a=b=0;
+    int a1, b1;
+    scanf("%d", &n1);
+    for(int i=0; i<n1; ++i)
+    {
+        scanf("%d", &a1);
+        a=(a | (1 << a1));
+    }
+    scanf("%d", &n2);
+    for(int i=0; i<n2; ++i)
+    {
+        scanf("%d", &b1);
+        b=(b | (1 << b1));
+    }
+    res= a & b;
+
+    for(int i=0; i<32; ++i)
+    {
+        if (checkbit(res, i) == 1)
+            printf("%d ", i);
+    }
+
+}
+
+
