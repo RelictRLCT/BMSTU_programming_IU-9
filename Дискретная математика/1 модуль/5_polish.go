cac20cc4d06719e92d436f75ca76ab907f8aadab
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2023-03-17 17:25:32 +0300

    Create 5_polish.go

---
+++ Дискретная математика/1 модуль/5_polish.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func args(str string) [2]string {
+	var (
+		op_br = 0
+		cl_br = 0
+		args  [2]string
+	)
+
+	if str[0] != '(' {
+		args[0] = string(str[0])
+	} else {
+		op_br += 1
+		for op_br != cl_br {
+			str = str[1:]
+			if str[0] == '(' {
+				op_br += 1
+			}
+			if str[0] == ')' {
+				cl_br += 1
+			}
+			args[0] += string(str[0])
+		}
+	}
+	op_br, cl_br = 0, 0
+
+	str = str[2:]
+	if str[0] != '(' {
+		args[1] = string(str[0])
+	} else {
+		op_br += 1
+		for op_br != cl_br {
+			str = str[1:]
+			if str[0] == '(' {
+				op_br += 1
+			}
+			if str[0] == ')' {
+				cl_br += 1
+			}
+			args[1] += string(str[0])
+		}
+	}
+	return args
+}
+
+func SCHEME(str string) int {
+	if str[0] == '+' {
+		args := args(str[2:])
+		return SCHEME(args[0]) + SCHEME(args[1])
+	} else if str[0] == '-' {
+		args := args(str[2:])
+		return SCHEME(args[0]) - SCHEME(args[1])
+	} else if str[0] == '*' {
+		args := args(str[2:])
+		return SCHEME(args[0]) * SCHEME(args[1])
+	} else if str[0] == ' ' || str[0] == '(' || str[0] == ')' {
+		return SCHEME(str[1:])
+	} else {
+		return int(str[0]) - 48 
+	} 
+} 
+
+func main() {
+	var str string
+
+	scanner := bufio.NewScanner(os.Stdin) 
+	scanner.Scan()                        
+	str = scanner.Text()                  
+
+	fmt.Print(SCHEME(str))
+}
