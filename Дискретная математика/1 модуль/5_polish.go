4921905ec5d10d7833c2dfc08bd9264be63e55dd
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2023-05-13 15:21:46 +0300

    Update 5_polish.go

--- Дискретная математика/1 модуль/5_polish.go
+++ Дискретная математика/1 модуль/5_polish.go
@@ -13,6 +13,10 @@ func args(str string) [2]string {
 		args  [2]string
 	)
 
+	for str[0] == ' ' {
+		str = str[1:]
+	}
+
 	if str[0] != '(' {
 		args[0] = string(str[0])
 	} else {
@@ -25,12 +29,18 @@ func args(str string) [2]string {
 			if str[0] == ')' {
 				cl_br += 1
 			}
-			args[0] += string(str[0])
+			if str[0] != ' ' {
+				args[0] += string(str[0])
+			}
 		}
 	}
 	op_br, cl_br = 0, 0
 
-	str = str[2:]
+	str = str[1:]
+	for str[0] == ' ' {
+		str = str[1:]
+	}
+
 	if str[0] != '(' {
 		args[1] = string(str[0])
 	} else {
@@ -51,27 +61,27 @@ func args(str string) [2]string {
 
 func SCHEME(str string) int {
 	if str[0] == '+' {
-		args := args(str[2:])
+		args := args(str[1:])
 		return SCHEME(args[0]) + SCHEME(args[1])
 	} else if str[0] == '-' {
-		args := args(str[2:])
+		args := args(str[1:])
 		return SCHEME(args[0]) - SCHEME(args[1])
 	} else if str[0] == '*' {
-		args := args(str[2:])
+		args := args(str[1:])
 		return SCHEME(args[0]) * SCHEME(args[1])
 	} else if str[0] == ' ' || str[0] == '(' || str[0] == ')' {
 		return SCHEME(str[1:])
 	} else {
-		return int(str[0]) - 48 
-	} 
-} 
+		return int(str[0]) - 48
+	}
+}
 
 func main() {
 	var str string
 
-	scanner := bufio.NewScanner(os.Stdin) 
-	scanner.Scan()                        
-	str = scanner.Text()                  
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	str = scanner.Text()
 
 	fmt.Print(SCHEME(str))
 }
