6186d8040e9effca15793149409d55680100cc5d
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2023-05-14 20:20:24 +0300

    Update 3_fibsys.c

--- algorithms/1.Изучение языка C/3_fibsys.c
+++ algorithms/1.Изучение языка C/3_fibsys.c
@@ -8,7 +8,7 @@ unsigned long long int fibar(unsigned long long int *fib, unsigned long long int
     if (x>0) fib[0]=1;
     if (x>1) fib[1]=1;
     unsigned long long int i=2;
-    while(true)
+    while (true)
     {
         fib[i]=fib[i-1]+fib[i-2];
         if(fib[i] >= x) break;
@@ -27,11 +27,12 @@ int main(int argc, char* argv[])
 
    for(unsigned long long int i=0; i<500; ++i)
     {
-        fib[i]=0;
-        fibsys[i]=0;
+    fib[i]=0;
+    fibsys[i]=0;
     }
     index=fibar(fib, x);
     unsigned long long int i=0;
+
     j=0;
     for(; index>0; --index)
     {
@@ -44,26 +45,30 @@ int main(int argc, char* argv[])
             }
 
         if (fib[index]<=x)
-        {
-            fibsys[j]=1;
-            x-=fib[index];
-            j+=1;
-        }
-        else {j+=1; continue;}
+         {
+                fibsys[j]=1;
+                x-=fib[index];
+                j+=1;
+         }
+         else {j+=1; continue;}
 
     }
+
+
     if (x1==0)
         printf("%d", 0);
-    else if (x1==1)
-        printf("%d", 1);
+   else if (x1==1)
+    printf("%d", 1);
     else
-    for(unsigned long long int i=0; i<j; ++i)
+   for(unsigned long long int i=0; i<j; ++i)
+
     {
         if((i==0) and (fibsys[i]==0))
             continue;
         printf("%lld", fibsys[i]);
     }
-    free(fib);
-    free(fibsys);
+
+  free(fib);
+  free(fibsys);
     return 0;
 }
