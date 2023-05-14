4687cc705e63a4dc4bd5845869fb89d39f42cdae
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2023-05-14 20:19:40 +0300

    Update 2_mulmod.c

--- algorithms/1.Изучение языка C/2_mulmod.c
+++ algorithms/1.Изучение языка C/2_mulmod.c
@@ -4,33 +4,34 @@
 
 int main(int argc, char* argv[])
 {
-    unsigned long long int a, b, m;
-    scanf("%lld %lld %lld", &a, &b, &m);
-    int B2[64]={0};
-    int i=-1;
-    while(b>0)
-    {
-        i+=1;
-        B2[i]= (b%2) ;
-        b/=2;
-    }
-    int B22[i+1];
-    int j=-1;
-    for(; i>=0; --i)
-    {
-        j+=1;
-        B22[j]=B2[i];
-    }
+        unsigned long long int a, b, m;
+        scanf("%lld %lld %lld", &a, &b, &m);
+        int B2[64]={0};
+        int i=-1;
+        while(b>0)
+        {
+            i+=1;
+            B2[i]= (b%2) ;
+            b/=2;
 
-    unsigned long long int res=0;
+        }
+        int B22[i+1];
+        int j=-1;
+     for(; i>=0; --i)
+        {
+             j+=1;
+          B22[j]=B2[i];
+        }
 
-    for(int i=0; i<j; ++i)
-    {
-        res=((res+a*B22[i])%m)*2;
-    }
-    res=res+(B22[j]*a)%m;
-    res%=m;
-    printf("%lld", res);
+         unsigned long long int res=0;
 
-    return 0;
+         for(int i=0; i<j; ++i)
+            {
+                res=((res+a*B22[i])%m)*2;
+            }
+            res=res+(B22[j]*a)%m;
+            res%=m;
+            printf("%lld", res);
+
+        return 0;
 }
