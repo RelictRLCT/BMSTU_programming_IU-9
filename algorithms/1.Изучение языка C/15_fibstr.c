1f0545f2e6b83accc601bc977b7057cfc6c70107
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2023-05-14 20:24:41 +0300

    Update 15_fibstr.c

--- algorithms/1.Изучение языка C/15_fibstr.c
+++ algorithms/1.Изучение языка C/15_fibstr.c
@@ -14,9 +14,7 @@ char *fibstr(int n)
     {
         strs=(char**)realloc(strs, sizeof(char*)*(i+1));
         strs[i]=(char*)malloc(strlen(strs[i-2]) + strlen(strs[i-1]) + 1);
-
         char* tmp=(char*)malloc(strlen(strs[i-1])+strlen(strs[i-2]) +1);
-
         memcpy(tmp, strs[i-2], strlen(strs[i-2]));
         memcpy(tmp+strlen(strs[i-2]), strs[i-1], strlen(strs[i-1])+1);
         strcpy(strs[i], tmp);
@@ -31,18 +29,18 @@ char *fibstr(int n)
     }
     else if (n==2)
     {
-         str=(char*)malloc(2);
+        str=(char*)malloc(2);
         str[0]='b';
         str[1]='\0';
     }
     else
-        {
-            str=(char*)malloc((strlen(strs[n-2])+strlen(strs[n-1])+1));
-            strcpy(str, strs[n-1]);
-        }
+    {
+        str=(char*)malloc((strlen(strs[n-2])+strlen(strs[n-1])+1));
+        strcpy(str, strs[n-1]);
+    }
     for(int i=2; i<n; ++i)
         free(strs[i]);
-     free(strs);
+    free(strs);
     return str;
 }
 
