6d492acc7b718c321a2793fea3bf2d99c1fb1a9f
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2023-05-14 20:25:37 +0300

    Update 4_shellsort.c

--- algorithms/2.Алгоритмы сортировки и поиска/4_shellsort.c
+++ algorithms/2.Алгоритмы сортировки и поиска/4_shellsort.c
@@ -1,46 +1,39 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <iso646.h>
+#include <stdlib.h>
 
 void shellsort(unsigned long nel,
         int (*compare)(unsigned long i, unsigned long j),
         void (*swap)(unsigned long i, unsigned long j))
 {
-    int FIB1[nel];
-    FIB1[0]=FIB1[1]=1;
-    int i=2;
-    while(true)
+    int* fib=(int*)malloc(sizeof(int)*2);
+    fib[0]=fib[1]=1;
+    int i=1;
+    while(fib[i]<nel)
     {
-        FIB1[i]=FIB1[i-1]+FIB1[i-2];
-        if (FIB1[i]>=nel) break;
         i+=1;
+        fib=(int*)realloc(fib, sizeof(int)*(i+1));
+        fib[i]=fib[i-2]+fib[i-1];
     }
-    int FIB[i-1];
-    for(; i<nel; ++i)
+
+    int F[i-1];
+    for(int j=0; j<i-1; ++j)
+        F[j]=fib[i-1-j];
+
+    for(int j=0; j<i-1; ++j)
     {
-        FIB1[i]=-1;
-    }
-    int j=0;
-    for(i=i-1; i>0; --i)
-    {
-        if (FIB1[i]!=-1)
-        {
-            FIB[j]=FIB1[i];
-            j+=1;
-        }
-    }
-    int fibm=j;
-    int k;
-    for(int i=0; i<fibm; ++i)
-        for(int j=FIB[i]; j<nel; j+=FIB[i])
-        {
-            k=j-FIB[i];
-            while(k>=0)
+        int ii=F[j];
+            while(ii<nel)
             {
-                if ((compare (j, k))==1 )
-                    {
-                        swap(k, j);
-                    }
-                k-=FIB[i];}
-        }
+                int k=ii;
+                while(k-F[j]>=0 and compare(k-F[j], k)==1)
+                {
+                    swap(k-F[j], k);
+                    k-=F[j];
+                }
+                ii+=F[j];
+            }
+    }
+    free(fib);
 }
