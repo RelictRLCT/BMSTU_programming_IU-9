53f5694ba326648d6c3f6a349c53eb2f73bf4bf6
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2023-09-17 22:03:58 +0300

    Create implementation.cpp

---
+++ Языки и методы программирования/ЛАБА 8/implementation.cpp
@@ -0,0 +1,26 @@
+#include "iostream"
+#include "declaration.h"
+using namespace std;
+/*
+if(reversed){
+reversed = false;
+head +=1;
+tail +=1;
+if( head > cap -1){
+head = 0;
+}
+if( tail > cap -1){
+tail = 0;
+}
+} else {
+reversed = true;
+head -=1;
+tail -=1;
+if( head < 0){
+head = cap-1;
+}
+if( tail < 0){
+tail = cap-1;
+}
+}
+}*/
