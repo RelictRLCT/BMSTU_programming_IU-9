1ffa30a89cf2bf33b809cb2fed73b877a96aebb4
RelictRLCT <121343776+RelictRLCT@users.noreply.github.com>
2024-08-15 13:01:42 +0300

    Add files via upload

---
+++ Компьютерные сети/Лабораторная 1.1/client.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/skorobogatov/input"
+	"net"
+)
+
+// interact - функция, содержащая цикл взаимодействия с сервером.
+func interact(conn *net.TCPConn) {
+	defer conn.Close()
+	encoder, decoder := json.NewEncoder(conn), json.NewDecoder(conn)
+	for {
+		// Чтение команды из стандартного потока ввода
+		fmt.Printf("command = ")
+		command := input.Gets()
+
+		// Отправка запроса.
+		switch command {
+		case "quit":
+			send_request(encoder, "quit", nil)
+			return
+		case "add":
+			var elem Elem
+			fmt.Printf("элемент = ")
+			elem.X = input.Gets()
+			send_request(encoder, "add", &elem)
+		case "sort":
+			send_request(encoder, "sort", nil)
+		default:
+			fmt.Printf("error: unknown command\n")
+			continue
+		}
+
+		// Получение ответа.
+		var resp Response
+
+		if err := decoder.Decode(&resp); err != nil {
+			fmt.Printf("error: %v\n", err)
+			break
+		}
+
+		// Вывод ответа в стандартный поток вывода.
+		switch resp.Status {
+		case "ok":
+			fmt.Printf("ok\n")
+		case "failed":
+			if resp.Data == nil {
+				fmt.Printf("error: data field is absent in response\n")
+			} else {
+				var errorMsg string
+				if err := json.Unmarshal(*resp.Data, &errorMsg); err != nil {
+					fmt.Printf("error: malformed data field in response\n")
+				} else {
+					fmt.Printf("failed: %s\n", errorMsg)
+				}
+			}
+		case "result":
+			if resp.Data == nil {
+				fmt.Printf("error: data field is absent in response\n")
+			} else {
+				var arr []int
+				if err := json.Unmarshal(*resp.Data, &arr); err != nil {
+					fmt.Printf("error: malformed data field in response\n")
+				} else {
+					fmt.Println(arr)
+				}
+			}
+		default:
+			fmt.Printf("error: server reports unknown status %q\n", resp.Status)
+		}
+	}
+}
+
+// send_request - вспомогательная функция для передачи запроса с указанной командой
+// и данными. Данные могут быть пустыми (data == nil).
+func send_request(encoder *json.Encoder, command string, data interface{}) {
+	var raw json.RawMessage
+	raw, _ = json.Marshal(data)
+	encoder.Encode(&Request{command, &raw})
+}
+
+func main() {
+	// Работа с командной строкой, в которой может указываться необязательный ключ -addr.
+	var addrStr string
+	flag.StringVar(&addrStr, "addr", "127.0.0.1:6000", "specify ip address and port")
+	flag.Parse()
+
+	// Разбор адреса, установка соединения с сервером и
+	// запуск цикла взаимодействия с сервером.
+	if addr, err := net.ResolveTCPAddr("tcp", addrStr); err != nil {
+		fmt.Printf("error: %v\n", err)
+	} else if conn, err := net.DialTCP("tcp", nil, addr); err != nil {
+		fmt.Printf("error: %v\n", err)
+	} else {
+		interact(conn)
+	}
+}
---
+++ Компьютерные сети/Лабораторная 1.1/go.mod
@@ -0,0 +1,12 @@
+module 1
+
+go 1.21
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab // indirect
+	github.com/skorobogatov/input v0.0.0-20130306234943-1101603a3b41 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
