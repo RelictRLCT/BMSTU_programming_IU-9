3b83618b260487fefbe7092ddb095935de46d55e
RELICT <121343776+RelictRLCT@users.noreply.github.com>
2024-10-06 12:57:19 +0300

    Create systems_creation.h

---
+++ Разработка параллельных и распределённых программ/Лабораторная 2/lab2/systems/systems_creation.h
@@ -0,0 +1,15 @@
+#ifndef LAB2CPP_SYSTEMS_CREATION_H
+#define LAB2CPP_SYSTEMS_CREATION_H
+
+#endif //LAB2CPP_SYSTEMS_CREATION_H
+
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void create_system_test_1(int, vector<vector<double>>&, vector<double>&);
+
+void create_system_test_2(int, vector<vector<double>>&, vector<double>&);
+
+void create_system_random(int, vector<vector<double>>&, vector<double>&);
